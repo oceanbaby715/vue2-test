ddc22dce671adceff69cf9549ff00c200f24c533
oceanbaby715 <liuhaiyang715@163.com>
2017-12-26 09:42:14 +0800

    test

--- Vue-cli/vuecli-test/src/App.vue
+++ Vue-cli/vuecli-test/src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <div>
+      <router-link to="/">首页</router-link> |
+      <router-link to="/hi">Hi页面</router-link> |
+      <router-link :to="{name:'hi1',params:{username:'terry',id:'715'}}">Hi页面1</router-link> |
+      <router-link to="/hi/hi2">Hi页面2</router-link>
+    </div>
+    <p>{{ $route.name }}</p>
+    <router-view></router-view>
   </div>
 </template>
 
